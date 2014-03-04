95ea6bca48ec2b1b5e2486add485bc1e576d45a1
Maxwell Dergosits <mad293@cornell.edu>
2014-03-04 12:27:07 -0500

    added logging util

---
+++ utils/log_utils.go
@@ -0,0 +1,25 @@
+package utils
+
+
+import (
+  "log"
+)
+
+
+
+
+func gen_logger(verbose bool, file string) (printer func(a ...interface{}), verbosePrinter func(a ...interface{})) {
+  var ver func(a ...interface{})
+  if verbose {
+    ver = func(a ...interface{}) {
+      log.Println(a)
+
+    }
+  } else {
+    ver = func(a ...interface{}) { }
+  }
+
+  var norm = func(a ...interface{}) { log.Println(a)}
+  return norm, ver
+
+}
