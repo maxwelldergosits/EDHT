06ad66f6de32ae466795f2cf5121a04577ab1899
SonofBSOD <SonofBSOD@users.noreply.github.com>
2014-03-09 21:53:48 -0400

    Update daemon-data-store.go
    Baseline code for daemon. IMPORTANT: the code HAS NOT been tested. Testing will soon follow as I smooth some bumps with the go installation on my computer. I expect to have a fully functioning version fixed and uploaded by tomorrow night.

--- daemon/daemon-data-store.go
+++ daemon/daemon-data-store.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"os"
 	"log"
 	"strconv"
 	"errors"
   . "EDHT/common"
 )
 
-var hashtable Hashtable= Hashtable{0,make(map[string]string)}
+//var hashtable Hashtable= Hashtable{0,make(map[string]string)} deprecated; each Daemon has it's own store now.
 
 
 //insert value in tuple into hashtable.
@@ -24,7 +25,7 @@ func lookup(key string) (string, error){
 	return "", errors.New("daemon lookup error: nonexistent key.")
 }
 
-/*Returns a pointer to a newly constructed Daemon object.
+/*Returns a pointer to a newly constructed Daemon object, with the specified address and port number.
 */
 func SpawnDaemon(address string, port string) *RemoteServer{
 	remote_server := RemoteServer{Address:address, Port:port, ID:0, Coordinator:false}
@@ -32,8 +33,13 @@ func SpawnDaemon(address string, port string) *RemoteServer{
 	return &Daemon{ServerDetails:remote_server, hashtable:table}
 }
 
-func RegisterDaemon(coordinator_ip string, coordinator_port string) error{
-
+/*Given the ip address and port for a known coordinator, will attempt to connect 
+ *to the coordinator with the provided ip address and port number.
+ *TODO: test error condition with AttachToGroup
+*/
+func (t *Daemon) RegisterDaemon(coordinator_ip string, coordinator_port string) error{
+	return AttachToGroup(coordinator_ip, coordinator_port)
+	
 }
 
 /*Given a DataPair struct, Put will associate the member "key" with member "value" in the daemon's data store.
@@ -41,7 +47,7 @@ func RegisterDaemon(coordinator_ip string, coordinator_port string) error{
  *associated with the most recent value. The function's return value
  *is non-nil if the storage is successful. The empty string is not accepted as a valid key and
  *will result in Put failure. */
-func (t *RemoteServer) Put(pair DataPair, reply *string) error {
+func (t *Daemon) Put(pair DataPair, reply *string) error {
 	if(pair.Value == "") {
 		return errors.New("daemon Put error: empty key")
 	}
@@ -55,7 +61,7 @@ func (t *RemoteServer) Put(pair DataPair, reply *string) error {
  *the result of the lookup operation. The return value of the function will indicated whether the lookup
  *succeeded; as stated in Go RPC Semantics, a non-nil error value (i.e. key is not in store) will not
  *return a result in the reply parameter.*/
-func (t *RemoteServer) Get(key string, reply *string) error {
+func (t *Daemon) Get(key string, reply *string) error {
 	val, err := lookup(key)
 	*reply = val
 	return err
@@ -64,11 +70,37 @@ func (t *RemoteServer) Get(key string, reply *string) error {
 /*GetAllKeys will return an array consisting of all keys in the daemon's data store in arbitrary order. 
  *Argument arg is ignored, and reply is not used. Error is nil on success, non-nil on failure.
  */
-func (t *RemoteServer) GetAllKeys(arg string, reply *string) error{
-	var keys[
+func (t *Daemon) GetAllKeys(arg string, reply *[]string) error{
+	keys := make([]string, len(t.Store))
+	i := 0
+	for key, _ := range t.Store{
+		keys[i] := key
+	}
+	*reply = keys
+	return nil
 }
 
+/*WriteToDisk will attempt to dump the entire contents of the daemon's data store into a file 
+ *named by "path." As discussed on Friday, "path" will temporarily take the name of a local file
+ *in the same directory, and will eventually be changed to provide support for absolute paths.
+ *Error is nil on success, non-nil on failure; Reply is not used.
+*/
 
-func (t *RemoteServer) WriteToDisk(path string, reply *string) error{
-
+func (t *Daemon) WriteToDisk(path string, reply *string) error{
+	f, err := os.Create("path")
+	if(err != nil){
+		return errors.New("WriteToDisk: OS file error.")
+	}
+	
+	defer f.Close()
+	
+	for key, value := range t.Store{
+		_, err := f.WriteString(key + ", " + value)
+		if(err != nil){
+			return errors.New("WriteToDisk: WriteString error.")
+		}
+	}
+	
+	f.Sync()
+	return nil
 }
