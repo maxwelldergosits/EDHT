1fbe4695ab0f1f250beb202c2a4fb0cb2203df9d
Kluchy <kfm53@cornell.edu>
2014-05-02 12:17:10 -0400

    Updated Test + Results/Statistics
    "n" in request_test.py is now sys.argv[3]

--- request_test.py
+++ request_test.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 div = ((2 ** 64) - 1) / 100
-n = 10000
+n = int( sys.argv[3] )
 coordinator_put_link = sys.argv[1]
 coordinator_get_link = sys.argv[2]
 
@@ -15,7 +15,7 @@ averageRequestTime= 0.0
 for i in range(0, n):
   #form_wrapper = {"key": struct.pack("L", div * i), "value" : str(i)}
   form_wrapper = {"key": str(div*i) , "value" : str(i)}
-  print form_wrapper
+  #print form_wrapper
   start= time.clock()
   request_object = requests.get(coordinator_put_link, params=form_wrapper)
   end= time.clock()
@@ -26,22 +26,40 @@ for i in range(0, n):
       longestRequestTime= duration
   averageRequestTime+= duration
   
-  print request_object.url
+  #print request_object.url
   time.sleep(.01)
   
 averageRequestTime= averageRequestTime / n
-print "shortest request time: " + str(shortestRequestTime)
-print "longest request time: " + str(longestRequestTime)
-print "average request time: " + str(averageRequestTime)
+
 
 #check the stored values
 oldValues= {}
+numErrors= 0
+shortestGetTime= 9999999999.0
+longestGetTime= 0.0
+averageGetTime= 0.0
 for i in range(0, n):
   #request_object = requests.get(coordinator_put_link, params = {"key" : struct.pack("L", div * i)})
+  start= time.clock()
   request_object = requests.get(coordinator_get_link, params = {"key" : str(div*i) })
+  end= time.clock()
+  duration= end - start
+  if duration < shortestGetTime:
+      shortestGetTime= duration
+  if duration > longestGetTime:
+      longestGetTime= duration
+  averageGetTime+= duration
+  
   value_object = json.loads(request_object.text)
-  assert value_object["value"] == str(i)
-  oldValues[str(div*i)]= str(i)
+  try:
+    assert value_object["value"] == str(i)
+    oldValues[str(div*i)]= str(i)
+  except AssertionError:
+      numErrors+=1
+      print "expected value: " + str(i) 
+      print "obtained value: " + value_object["value"]
+      
+averageGetTime= averageGetTime / n
 
 #now, update values
 for key in oldValues:
@@ -50,7 +68,25 @@ for key in oldValues:
     time.sleep(.01)
 
 #check if new values returned
+numNewErrors= 0
 for key in oldValues:
     request_object= requests.get(coordinator_get_link, params= {"key" : key} )
-    value_object= json.loads(request_object.text)
-    assert value_object["value"] == oldValues[key] + "||" + key
+    value_object= json.loads(request_object.text) 
+    try:
+        assert value_object["value"] == oldValues[key] + "||" + key
+    except AssertionError:
+        numNewErrors+=1
+        print "expected new value: " + oldValues[key] + "||" + key
+        print "obtained value: " + value_object["value"]
+        
+#print statistics
+print ("shortest put request time: %.2f ms" % (1000 * shortestRequestTime)) #+ str(1000 * shortestRequestTime)
+print ("longest put request time: %.2f ms" % (1000 * longestRequestTime) )
+print ("average put request time: %.2f ms" % (1000 * averageRequestTime) )
+print (" ")
+print ("shortest get request time: %.2f ms" % (1000 * shortestGetTime) )
+print ("longest get request time: %.2f ms" % (1000 * longestGetTime) )
+print ("average get request time: %.2f ms" % (1000 * averageGetTime) )
+print (" ")
+print "assertion errors on initial gets: " + str(100 * numErrors / float(n) ) + "%"
+print "assertion errors on updates: " + str(100 * numNewErrors / float(n) ) + "%"
